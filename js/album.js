2f4174ff98846843ccc2819426c1321692a1ed8c
maltelessing <malte.lessing@student.tabyenskilda.se>
2023-05-10 19:43:52 +0200

    slutprojekt 10 Maj
    Filmsida ish klar

--- js/album.js
+++ js/album.js
@@ -76,32 +76,3 @@ function nerMove2() {
     }
   }
 } 
-/* var animation1 = document.getElementById("myAnimation");
-var container1 = document.getElementById("myContainer");
-var animation2 = document.getElementById("myAnimation2");
-var container2 = document.getElementById("myContainer2");
-
-var height = 70; // Antal pixlar att röra animationen uppåt
-
-function moveAnimation1() {
-  animation1.style.top = "-" + height + "px";
-}
-
-function resetAnimation1() {
-  animation1.style.top = "0px";
-}
-
-function moveAnimation2() {
-  animation2.style.top = "-" + height + "px";
-}
-
-function resetAnimation2() {
-  animation2.style.top = "0px";
-}
-
-container1.addEventListener("mouseover", moveAnimation1);
-container1.addEventListener("mouseout", resetAnimation1);
-
-container2.addEventListener("mouseover", moveAnimation2);
-container2.addEventListener("mouseout", resetAnimation2);
-*/
