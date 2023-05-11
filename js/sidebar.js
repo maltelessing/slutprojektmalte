9c90a800d13df8a0f98debb556a5f9dfaf4cdac5
maltelessing <malte.lessing@student.tabyenskilda.se>
2023-05-11 10:06:56 +0200

    Slutprojekt pkk.se 11 maj

--- js/sidebar.js
+++ js/sidebar.js
@@ -5,3 +5,37 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+
+  window.addEventListener('scroll', showRutorna);
+
+  function showRutorna() {
+    var rutorna = document.querySelectorAll('.rutorna > div');
+  
+    for (var i = 0; i < rutorna.length; i++) {
+      var ruta = rutorna[i];
+      var rutaTop = ruta.getBoundingClientRect().top;
+  
+      if (rutaTop < window.innerHeight) {
+        ruta.classList.add('show');
+      } else {
+        ruta.classList.remove('show');
+      }
+    }
+  }
+/* Menyn-så den håller sig uppe hela tiden */
+window.addEventListener('scroll', function() {
+  var menuText = document.querySelector('.menu-text');
+  var distanceFromTop = menuText.getBoundingClientRect().top;
+  var threshold = 100; /* Justera tröskelvärdet efter behov */
+  var menuOverlay = document.querySelector('.menu-overlay');
+
+  if (distanceFromTop <= threshold) {
+    menuText.classList.add('fixed');
+    menuOverlay.classList.add('fixed-overlay');
+  } else {
+    menuText.classList.remove('fixed');
+    menuOverlay.classList.remove('fixed-overlay');
+  }
+});
+
